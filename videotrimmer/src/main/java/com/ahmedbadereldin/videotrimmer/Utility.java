751e8641fe95b36ec511f390051f921df52c3819
rahulsain <50570677+rahulsain@users.noreply.github.com>
2022-03-01 11:57:18 +0530

    fixed video upload crash issue

--- videotrimmer/src/main/java/com/ahmedbadereldin/videotrimmer/Utility.java
+++ videotrimmer/src/main/java/com/ahmedbadereldin/videotrimmer/Utility.java
@@ -69,8 +69,8 @@ public class Utility {
 
                     throw new RuntimeException("The startTime has already been corrected by another track with SyncSample. Not Supported.");
                 }
-//                startTime1 = correctTimeToSyncSample(track, startTime1, false);
-//                endTime1 = correctTimeToSyncSample(track, endTime1, true);
+                startTime1 = correctTimeToSyncSample(track, startTime1, false);
+                endTime1 = correctTimeToSyncSample(track, endTime1, true);
                 timeCorrected = true;
             }
         }
@@ -116,6 +116,34 @@ public class Utility {
         }
 
     }
+
+    private static double correctTimeToSyncSample(Track track, double cutHere, boolean next) {
+        double[] timeOfSyncSamples = new double[track.getSyncSamples().length];
+        long currentSample = 0;
+        double currentTime = 0;
+        for (int i = 0; i < track.getSampleDurations().length; i++) {
+            long delta = track.getSampleDurations()[i];
+            if (Arrays.binarySearch(track.getSyncSamples(), currentSample + 1) >= 0) {
+                // samples always start with 1 but we start with zero therefore +1
+                timeOfSyncSamples[Arrays.binarySearch(track.getSyncSamples(), currentSample + 1)] = currentTime;
+            }
+            currentTime += (double) delta / (double) track.getTrackMetaData().getTimescale();
+            currentSample++;
+        }
+        double previous = 0;
+        for (double timeOfSyncSample : timeOfSyncSamples) {
+            if (timeOfSyncSample > cutHere) {
+                if (next) {
+                    return timeOfSyncSample;
+                } else {
+                    return previous;
+                }
+            }
+            previous = timeOfSyncSample;
+        }
+        return timeOfSyncSamples[timeOfSyncSamples.length - 1];
+    }
+
 
     private static File create(Activity activity, String dst) {
         File file = new File(dst);
